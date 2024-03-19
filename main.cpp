1039f3330c49cad5a48060315bb40c2e02e211ad
Matt Clarke <matt.clarke@ess.eu>
2024-03-19 08:13:54 +0100

    can write scalar and simple vector

--- main.cpp
+++ main.cpp
@@ -4,11 +4,29 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    std::filesystem::path path{"test.hdf"};
-    hdf5::property::FileAccessList file_access_list;
-    hdf5::property::FileCreationList file_creation_list;
+    std::filesystem::path path{"./test.hdf"};
 
-    hdf5::file::File file = hdf5::file::create(path, hdf5::file::AccessFlags::ReadWrite, file_creation_list,
-                                               file_access_list);
+    auto file = hdf5::file::create(path, hdf5::file::AccessFlags::Exclusive, {}, {});
+    auto root_group = file.root();
+
+    // Write a scalar value
+    {
+        auto dataspace = hdf5::dataspace::Scalar();
+        const hdf5::datatype::Integer data_type = hdf5::datatype::create<int32_t>();
+        auto dataset = root_group.create_dataset("example_scalar1", data_type, dataspace);
+        dataset.write(123, data_type, dataspace);
+    }
+
+    // Write an array of ints
+    {
+        std::vector<int32_t> data {1, 2, 3};
+        auto dataspace = hdf5::dataspace::Simple({3});
+        const hdf5::datatype::Integer data_type = hdf5::datatype::create<int32_t>();
+        auto dataset = root_group.create_dataset("example_simple1", data_type, dataspace);
+        dataset.write(data, data_type, dataspace);
+    }
+
+    file.close();
+
     return 0;
 }
